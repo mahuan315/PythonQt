c312f8e898b6d7f20fe7ad605654d758edb79be0
Saito Asuka <moonwell@vip.qq.com>
2023-04-29 01:46:57 +0800

    second push

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget
-from Ui_untitled import Ui_MainWindow  #导入你写的界面类
- 
- 
-class MyMainWindow(QMainWindow,Ui_MainWindow): #这里也要记得改
-    def __init__(self,parent =None):
-        """_summary_
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from Ui_untitled import Ui_MainWindow  # 导入你写的界面类
 
-        Args:
-            parent (_type_, optional): _description_. Defaults to None.
-        """
-        super(MyMainWindow,self).__init__(parent)
+
+class MyMainWindow(QMainWindow, Ui_MainWindow):  # 这里也要记得改
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
         if 1 != 2:
             print("Usage:")
-            
- 
+
+
 # if __name__ == "__main__":
 #     app = QApplication(sys.argv)
 #     myWin = MyMainWindow()
@@ -27,5 +22,3 @@ if __name__ == "__main__":
     myWin = MyMainWindow()
     myWin.show()
     sys.exit(app.exec_())
-    
-
